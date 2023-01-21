ba6222ff42b1264c07bc62337359ba4f94dd5f30
lucasaraujo1987 <lukass.araujo11@gmail.com>
2023-01-21 14:13:09 -0300

    Update OperadoresAritimeticos.java

--- Java/aula05/OperadoresAritimeticos/src/operadoresaritimeticos/OperadoresAritimeticos.java
+++ Java/aula05/OperadoresAritimeticos/src/operadoresaritimeticos/OperadoresAritimeticos.java
@@ -14,10 +14,19 @@ public class OperadoresAritimeticos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int n1 = 3;
-        int n2 = 5;
-        float m = (n1 + n2) / 2;
-        System.out.println("A média é " + m);
+//        int n1 = 3;
+//        int n2 = 5;
+//        float m = (n1 + n2) / 2;
+//        System.out.println("A média é " + m);
+        
+//        int numero = 5;
+//        int valor = 5 + ++numero;
+//        System.out.println(valor);
+//        System.out.println(numero);
+
+        int x = 4;
+        x += 2; // x = x + 2;
+        System.out.println(x);
     }
     
 }
