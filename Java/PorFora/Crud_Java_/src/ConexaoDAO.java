bab7c9eb034434f24e163011362be01fedd213f4
lucasaraujo1987 <lukass.araujo11@gmail.com>
2023-05-04 22:05:48 -0300

    Update ConexaoDAO.java

--- Java/PorFora/Crud_Java_/src/ConexaoDAO.java
+++ Java/PorFora/Crud_Java_/src/ConexaoDAO.java
@@ -1,12 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author lukas
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 public class ConexaoDAO {
     
+    
+    public Connection conectaBD(){
+        Connection conn = null;
+        
+        try {
+            String url = "jdbc:mysql://localhost:3306/bancoteste?user=root&password=";
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            JOptionPane.showConfirmDialog(null, e.getMessage());
+        }
+        return conn;
+    }
 }
