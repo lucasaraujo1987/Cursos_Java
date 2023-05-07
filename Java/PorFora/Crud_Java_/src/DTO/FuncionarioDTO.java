510d1a7932b0518944bb0e429789401101fe502c
lucasaraujo1987 <lukass.araujo11@gmail.com>
2023-05-06 23:44:08 -0300

    update

---
+++ Java/PorFora/Crud_Java_/src/DTO/FuncionarioDTO.java
@@ -0,0 +1,22 @@
+
+package DTO;
+public class FuncionarioDTO {
+    private String nome_funcionario, endereco_funcionario;
+
+    public String getNome_funcionario() {
+        return nome_funcionario;
+    }
+
+    public void setNome_funcionario(String nome_funcionario) {
+        this.nome_funcionario = nome_funcionario;
+    }
+
+    public String getEndereco_funcionario() {
+        return endereco_funcionario;
+    }
+
+    public void setEndereco_funcionario(String endereco_funcionario) {
+        this.endereco_funcionario = endereco_funcionario;
+    }
+    
+}
